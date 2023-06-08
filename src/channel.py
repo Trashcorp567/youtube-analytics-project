75bf9f96b7753604a505c9919b3ad9095a365911
Trashcorp567 <Trashcorp567@gmail.com>
2023-06-09 00:24:18 +0300

    homework2

--- src/channel.py
+++ src/channel.py
@@ -13,13 +13,57 @@ class Channel:
         Дальше все данные будут подтягиваться по API.
         """
         self.channel_id = channel_id
+        self.title = ""
+        self.description = ""
+        self.url = ""
+        self.subscriber_count = 0
+        self.video_count = 0
+        self.view_count = 0
+        self.fetch_channel_data()
 
+    def to_json(self, filename: str):
+        data = {
+            'channel_id': self.channel_id,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'video_count': self.video_count,
+            'view_count': self.view_count
+        }
+        with open(filename, 'w') as file:
+            json.dump(data, file)
+
+    def fetch_channel_data(self):
+        youtube = self.get_service()
+        channel = youtube.channels().list(
+            id=self.channel_id,
+            part='snippet,statistics'
+        ).execute()
+
+        if 'items' in channel:
+            channel_data = channel['items'][0]
+            snippet = channel_data['snippet']
+            statistics = channel_data['statistics']
+
+            self.title = snippet['title']
+            self.description = snippet['description']
+            self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+            self.subscriber_count = int(statistics['subscriberCount'])
+            self.video_count = int(statistics['videoCount'])
+            self.view_count = int(statistics['viewCount'])
+
+    @classmethod
+    def get_service(cls):
+        api_key = os.getenv('API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-
-        api_key = os.getenv('API_KEY')
-        youtube = build('youtube', 'v3', developerKey = api_key)
-        channel = youtube.channels().list(id = self.channel_id,
-        part = 'snippet,statistics').execute()
+        youtube = self.get_service()
+        channel = youtube.channels().list(
+            id=self.channel_id,
+            part='snippet,statistics'
+        ).execute()
         print(json.dumps(channel, indent = 2, ensure_ascii = False))
