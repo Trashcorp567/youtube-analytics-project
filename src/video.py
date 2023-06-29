c2be1355b49e6d2ad70e03bc294e715cddfbe760
Trashcorp567 <Trashcorp567@gmail.com>
2023-06-29 12:21:10 +0300

    homework6

--- src/video.py
+++ src/video.py
@@ -5,14 +5,15 @@ from googleapiclient.discovery import build
 class Video:
     def __init__(self, video_id: str) -> None:
         self.video_id = video_id
-        self.video_title = ""
-        self.video_url = ""
-        self.likes_count = ""
-        self.view_count = 0
+        self.title = None
+        self.video_url = None
+        self.like_count = None
+        self.view_count = None
+        self.comment_count = None
         self.fetch_video_data()
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
     @staticmethod
     def get_service():
@@ -21,21 +22,25 @@ class Video:
         return youtube
 
     def fetch_video_data(self):
-        youtube = self.get_service()
-        video = youtube.videos().list(
-            part='snippet,statistics',
-            id=self.video_id
-        ).execute()
+        try:
+            youtube = self.get_service()
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=self.video_id
+                                                   ).execute()
 
-        if 'items' in video:
-            video_data = video['items'][0]
-            snippet = video_data['snippet']
-            statistics = video_data['statistics']
+            self.title: str = video_response['items'][0]['snippet']['title']
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+            self.video_url: str = f"https://www.youtube.com/watch?v={self.video_id}"
 
-            self.video_title = snippet['title']
-            self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
-            self.view_count = int(statistics['viewCount'])
-            self.likes_count = int(statistics['likeCount'])
+        except:
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
+            self.video_url = None
+            #print(f'Неправильное id: {self.video_id}')
 
 
 class PLVideo(Video):
@@ -44,4 +49,4 @@ class PLVideo(Video):
         self.playlist_id = playlist_id
 
     def __str__(self):
-        return self.video_title
+        return self.title
